7f5d0d6231668eea06984d125c86fd9b9a0dcb35
envil a.k.a pow <86885041+abnvlf@users.noreply.github.com>
2024-02-18 19:16:08 +0700

    add: meta keywords, applicationName, creator, openGraph image (#69)
    * add: meta keywords, applicationName, creator, openGraph image
    
    * fix: change meta title

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,9 +11,37 @@ import "./globals.css";
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "OpenSeries",
+    title: "OpenSeries - Cheat Engine Matematika by Bellshade",
     description:
-        "Project Untuk Menghitung Segala Jenis Persamaan atau Rumus-Rumus yang terdapat pada bangku sekolah (SMA/SMK/Sederajat). Project ini bertujuan untuk memudahkan siswa dalam menghitung persamaan atau problem-problem yang terdapat pada pelajaran sekolah (cheat egine untuk Sekolah)."
+        "Project Untuk Menghitung Segala Jenis Persamaan atau Rumus-Rumus yang terdapat pada bangku sekolah (SMA/SMK/Sederajat). Project ini bertujuan untuk memudahkan siswa dalam menghitung persamaan atau problem-problem yang terdapat pada pelajaran sekolah (cheat egine untuk Sekolah).",
+    keywords: [
+        "OpenSeries",
+        "Bellshade",
+        "Bellshade OpenSeries",
+        "Library matematika",
+        "Library fisika",
+        "Library statistika",
+        "Library python matematika",
+        "Library python fisika",
+        "Library python statistika",
+        "Matematika SMA/SMK",
+        "Fisika SMA/SMK",
+        "Statistika SMA/SMK",
+        "Cheatsheet matematika",
+        "Cheatsheet fisika",
+        "Cheatsheet statistika",
+        "Python playground",
+        "Cheat engine SMA/SMK",
+        "Cheat engine matematika",
+        "Cheat engine fisika",
+        "Cheat engine statistika",
+        "OpenSeries python"
+    ],
+    applicationName: "OpenSeries",
+    creator: "Bellshade",
+    openGraph: {
+        images: ["/assets/img/banner.png"]
+    }
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
