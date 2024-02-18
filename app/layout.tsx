0b06b168d7c8f4991daa09c50540ca7dc261aa22
envil a.k.a pow <pow.zone@outlook.com>
2024-02-18 20:20:04 +0700

    fix: metadataBase url

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,8 @@ import "./globals.css";
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "OpenSeries - Cheat Engine Matematika by Bellshade",
+    metadataBase: new URL(process.env.NEXT_PUBLIC_URL || "http://localhost:3000"),
+    title: "OpenSeries - Cheat Engine Hitung-Hitungan by Bellshade",
     description:
         "Project Untuk Menghitung Segala Jenis Persamaan atau Rumus-Rumus yang terdapat pada bangku sekolah (SMA/SMK/Sederajat). Project ini bertujuan untuk memudahkan siswa dalam menghitung persamaan atau problem-problem yang terdapat pada pelajaran sekolah (cheat egine untuk Sekolah).",
     keywords: [
